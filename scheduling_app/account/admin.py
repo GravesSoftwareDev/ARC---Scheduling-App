e7ff6b75a94c67dd3db5f7382dc3688e102ca81c
Shanna <sg0989463@otc.edu>
2026-03-24 12:48:19 -0500

    Created Custom User

--- scheduling_app/account/admin.py
+++ scheduling_app/account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Employee
 
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role', {'fields': ('role',)}),
+    )
--- scheduling_app/account/models.py
+++ scheduling_app/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Employee(AbstractUser):
+    class Role(models.TextChoices):
+        employee = 'EMPLOYEE','Employee'
+        scheduler = 'SCHEDULER','Scheduler'
+        admin = 'ADMIN','Admin'
+    
+    role = models.CharField(
+        choices=Role.choices,
+        default=Role.employee
+    )
+
