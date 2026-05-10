d5d5531d00c78c56bf689d9048d2229b10ea27e8
Shanna <sg0989463@otc.edu>
2026-05-10 10:31:59 -0500

    Update Email SMTP

--- scheduling_app/scheduling_app/settings.py
+++ scheduling_app/scheduling_app/settings.py
@@ -111,6 +111,7 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = env_bool(os.environ.get('EMAIL_USE_TLS', 'True'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'graves_tech@outlook.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
